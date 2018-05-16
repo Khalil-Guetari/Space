e2dc24b4f0feaed1093f0173eb4486f269c7bbe6
HuguetPaul <35903346+HuguetPaul@users.noreply.github.com>
2018-05-16 15:32:33 +0200

    Update Exit_Level.java

--- Exit_Level.java
+++ Exit_Level.java
@@ -1,13 +1,14 @@
 package application;
 
-import javafx.scene.shape.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Exit_Level extends GameObject { 	//Classe désignant le point de sortie du niveau
+public class Exit_Level extends GameObject { 	//Classe dÃ©signant le point de sortie du niveau
 	
 												//Attributs pour le design de la sortie
 	
-	Exit_Level() {								//Constructeur --> un petit cercle rouge
-		super(new Rectangle(10, 15, javafx.scene.paint.Color.BLUE));
+	Exit_Level(Image image) {								//Constructeur --> un petit cercle rouge
+		super(new ImageView(image));
 	}								
 		
 	
